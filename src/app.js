1b35f8964d9ea0a8b0626ab9267334d6c254597e
Gustavo <gustavodarocha241@gmail.com>
2023-01-13 14:14:50 -0300

    iniciando Projeto #VQV

--- src/app.js
+++ src/app.js
@@ -6,8 +6,6 @@ const app = express();
 
 app.use(express.json());
 
-// ...
-
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
 module.exports = app;
